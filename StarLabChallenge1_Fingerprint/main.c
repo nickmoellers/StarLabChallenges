d701b67588112773e8dc8577188896bc33905357
Nicholas Moellers <nmperson@gmail.com>
2018-03-24 14:11:53 -0700

    Fingerprint is now time based and uses bitflags instead of malloc'ing bytes

--- StarLabChallenge1_Fingerprint/main.c
+++ StarLabChallenge1_Fingerprint/main.c
@@ -3,90 +3,63 @@
 #include <time.h>
 #include <sys/timeb.h>
 #include <string.h>
+#include <inttypes.h>
 
-#include <cpuid.h>
-#define _OPEN_SYS_EXT 1
-//#include <sys/ps.h>
+//static uint32_t fingerprint = 0; 
 
-static int fingerprint = -1;
-//static struct timeb *timeStruct
+void constructFingerPrint( uint32_t* fingerprint, uint32_t value, size_t numBits ) {
+  uint32_t bitFilter = 1;
+  bitFilter = (bitFilter<<numBits)-1;
+  //all zeros then all ones
+  bitFilter &= value;
+  //all zeroes then the data from value
 
-void calcFingerprintFunction( void ) {
-  char* fingerprint;
-  char* fingerprintPtr;
-  int size;
+  //printf("bitFilter = 0x%08" PRIx32 "\n", bitFilter);
 
-  int ret = -1;
-  clock_t myclock;
-  struct timeb mytime;
-
-  ret = ftime( &mytime );
-  myclock = clock();
-
-  /*printf("mytime = {\n \
-          \ttime_t\t%ld\n \
-          \tmillitm\t%hu\n \
-          \ttimezone\t%hd\n \
-          \tdstflag\t%hd\n \
-          }\n",
-          mytime.time, 
-          mytime.millitm,
-          mytime.timezone,
-          mytime.dstflag
-          );
-
-  //printf("sizeof(clocK)" %d, sizeof(clock));
-  printf("clock = %ld\n", (long int)myclock);
-
-  printf("\nsizes:\n");
-  printf("mytime = {\n \
-          \ttime_t\t%lu\n \
-          \tmillitm\t%lu\n \
-          \ttimezone\t%lu\n \
-          \tdstflag\t%lu\n \
-          }\n",
-          sizeof(mytime.time), 
-          sizeof(mytime.millitm),
-          sizeof(mytime.timezone),
-          sizeof(mytime.dstflag)
-          );
-  printf("clock = %lu\n", sizeof(myclock));
-  printf("char = %lu\n", sizeof(char));*/
-
-  size = 0;
-  size+=sizeof(mytime.time)/sizeof(char); //2
-  size+=sizeof(mytime.millitm)/sizeof(char); //8
-  size+=sizeof(myclock)/sizeof(char); //8
-
-  fingerprint = calloc( size, sizeof(char));
-  fingerprintPtr = fingerprint;
-  
-  size = sizeof(mytime.time)/sizeof(char); 
-  memcpy( fingerprintPtr, &mytime.time, size);
-  fingerprintPtr+=size;
-
-  size = sizeof(mytime.millitm)/sizeof(char); 
-  memcpy( fingerprintPtr, &mytime.millitm, size);
-  fingerprintPtr+=size;
-
-  size = sizeof(myclock)/sizeof(char); 
-  memcpy( fingerprintPtr, &myclock, size);
-  fingerprintPtr+=size;
-  
-  /*
-  printf("%016lx\t", myclock);
-  printf("%04x\t", mytime.millitm);
-  printf("%016lx\n", mytime.time);*/
-
-  while( (void*) fingerprint <= (void*)  --fingerprintPtr ) {
-    printf("%02x ", *fingerprintPtr & 0xff);
-  }
-  printf("\n");
+  *fingerprint=*fingerprint<<numBits;
+  //previous data then all zeroes
+  *fingerprint=*fingerprint | bitFilter;
+  //previous data then the new data
   return;
 }
 
-int fingerprintfunction() {
-	int fingerprint = 0;
+//Creates and returns a 32 bit number consisting of the following:
+//12 bits of CLOCK, then 10 bits of TIME, then 10 bits of MILLITIME
+uint32_t calcFingerprintFunction( void ) {
+  uint32_t fingerprint = 0; 
+  
+  clock_t myclock = clock();
+
+  struct timeb mytime;
+  ftime( &mytime );
+  
+  constructFingerPrint( &fingerprint, (unsigned int) myclock,        12 );
+  constructFingerPrint( &fingerprint, (unsigned int) mytime.time,    10 );
+  constructFingerPrint( &fingerprint, (unsigned int) mytime.millitm, 10 );
+  
+  //printf("%" PRIx32 "\n", fingerprint);
+
+  return fingerprint;
+}
+
+static uint32_t fingerprintFunction( void ) {
+  uint32_t fingerprint;
+  //open a file
+  //if it exists
+    //ead the fingerprint from the file
+    //return figerprint
+  //else
+    //calculated the fingerprint
+    fingerprint = calcFingerprintFunction();
+    //write the file
+      //if successful
+        return fingerprint;
+      //else
+      // error out
+  //end
+}
+
+	/*int fingerprint = 0;
 
 	for( int leaf = 0 ; leaf <= 4 ; leaf++ ) {
 		int eax = 3, ebx = 0, ecx = 0, edx = 0;
@@ -96,14 +69,12 @@ int fingerprintfunction() {
         }
 	}
 	return fingerprint;
-}
+}*/
 
 int main()
 {
-  calcFingerprintFunction();
-   // printf() displays the string inside quotation
-   //unsigned int fingerprint = 0;
-   //printf("Your fingerprint is %d", fingerprintfunction());
-   return 0;
+  uint32_t fingerprint = fingerprintFunction();
+  printf("%" PRIx32 "\n", fingerprint);
+  return 0;
 }
 
